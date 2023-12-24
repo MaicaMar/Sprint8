1804ff34b0859f4ad4858748c06260700630dd4e
Maica Mar <maicamcdg@gmail.com>
2023-12-24 02:16:43 +0100

    Components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { FullCalendarComponent } from './components/full-calendar/full-calendar.component';
+import { GraphicsComponent } from './components/graphics/graphics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'map',
+        component: MapComponent
+    },
+    {
+        path: 'full-calendar',
+        component: FullCalendarComponent
+    },
+    {
+        path: 'graphics',
+        component: GraphicsComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/home'
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+];
