b97c6f17ba9c162e74098ade007b7e6cfa4b42ca
Maica Mar <maicamcdg@gmail.com>
2024-01-09 09:18:54 +0100

    090124

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink], // Importamos el RouterLink de Angular para que funcione -por ejemplo- el routerLink="/home" de app.component.html y hay que hacer esto en cada componente que queramos mostrar en el html principal
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/full-calendar/full-calendar.component.ts
+++ src/app/full-calendar/full-calendar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-full-calendar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './full-calendar.component.html',
   styleUrl: './full-calendar.component.css'
 })
